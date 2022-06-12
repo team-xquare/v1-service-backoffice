df5490a8878c788143883473a3ff1aadb1444bb0
leeseojune53 <sung07288346@gmail.com>
2022-06-12 23:06:32 +0900

    📑 :: FeignConfig

---
+++ src/main/kotlin/io/github/v1servicebackoffice/infrastructure/feign/FeignConfig.kt
@@ -0,0 +1,20 @@
+package io.github.v1servicebackoffice.infrastructure.feign
+
+import feign.codec.ErrorDecoder
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@EnableFeignClients(basePackages = ["io.github.v1servicebackoffice.infrastructure.feign"])
+@Configuration
+class FeignConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(value = [ErrorDecoder::class])
+    fun commonFeignErrorDecoder(): FeignClientErrorDecoder {
+        return FeignClientErrorDecoder()
+    }
+
+}
