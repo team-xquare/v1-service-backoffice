c4ca36fed1ba50e8fdb45f9b3dfd6dd764e0309d
leeseojune53 <sung07288346@gmail.com>
2022-06-22 22:22:23 +0900

    ⚡️ :: Record findByName

--- src/main/kotlin/io/github/v1servicebackoffice/domain/record/domain/repository/RecordRepository.kt
+++ src/main/kotlin/io/github/v1servicebackoffice/domain/record/domain/repository/RecordRepository.kt
@@ -2,7 +2,7 @@ package io.github.v1servicebackoffice.domain.record.domain.repository
 
 import io.github.v1servicebackoffice.domain.record.domain.RecordEntity
 import org.springframework.data.repository.CrudRepository
-import java.util.UUID
 
-interface RecordRepository: CrudRepository<RecordEntity, UUID> {
+interface RecordRepository: CrudRepository<RecordEntity, String> {
+    fun findByName(name: String): RecordEntity?
 }
