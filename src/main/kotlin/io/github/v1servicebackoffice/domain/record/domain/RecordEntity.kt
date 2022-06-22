0eabcfb03b7fabe7eea4355d8a3ca24754209a31
leeseojune53 <sung07288346@gmail.com>
2022-06-22 18:49:26 +0900

    📑 :: RecordEntity

---
+++ src/main/kotlin/io/github/v1servicebackoffice/domain/record/domain/RecordEntity.kt
@@ -0,0 +1,24 @@
+package io.github.v1servicebackoffice.domain.record.domain
+
+import io.github.v1servicebackoffice.domain.record.domain.types.RecordType
+import io.github.v1servicebackoffice.global.entity.BaseUUIDEntity
+import java.util.*
+import javax.persistence.*
+
+
+@Table(name = "tbl_record")
+@Entity
+class RecordEntity(
+    override val id: UUID = UUID(0, 0),
+
+    @field:Column(length = 20)
+    val name: String,
+
+    @field:Column(length = 15)
+    val content: String,
+
+    @field:Enumerated(EnumType.STRING)
+    @field:Column(length = 20)
+    val type: RecordType
+
+): BaseUUIDEntity(id)
