bdee6f0ceb2c214c093152ac6902b288f3d604ae
leeseojune53 <sung07288346@gmail.com>
2022-06-23 09:00:39 +0900

    ⚡️ :: ErrorResponse constructor

--- src/main/kotlin/io/github/v1servicebackoffice/global/error/ErrorResponse.kt
+++ src/main/kotlin/io/github/v1servicebackoffice/global/error/ErrorResponse.kt
@@ -3,14 +3,13 @@ package io.github.v1servicebackoffice.global.error
 import io.github.v1servicebackoffice.global.error.ErrorCode
 
 class ErrorResponse(
-    errorCode: ErrorCode
+    private val errorCode: ErrorCode,
+    private val message: String = errorCode.message,
 ) {
 
     private val status: Int = errorCode.status
 
     private val code: String = errorCode.code
-
-    private val message: String = errorCode.message
 
     override fun toString(): String {
         return "{\n" +
