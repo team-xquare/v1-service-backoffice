2e3637452d2c307f9b865e254a550b6260aeb5a4
leeseojune53 <sung07288346@gmail.com>
2022-06-24 16:33:40 +0900

    ⚡️ :: Jackson allow

--- src/main/kotlin/io/github/v1servicebackoffice/global/error/ErrorResponse.kt
+++ src/main/kotlin/io/github/v1servicebackoffice/global/error/ErrorResponse.kt
@@ -1,22 +1,16 @@
 package io.github.v1servicebackoffice.global.error
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import io.github.v1servicebackoffice.global.error.ErrorCode
 
 class ErrorResponse(
-    private val errorCode: ErrorCode,
-    private val message: String = errorCode.message,
+    @JsonIgnore
+    val errorCode: ErrorCode,
+    val message: String = errorCode.message,
 ) {
 
-    private val status: Int = errorCode.status
+    val status: Int = errorCode.status
 
-    private val code: String = errorCode.code
-
-    override fun toString(): String {
-        return "{\n" +
-                "\t\"status\": " + status +
-                ",\t\"code\": \"" + code + '\"' +
-                ",\n\t\"message\": \"" + message + '\"' +
-                "\n}";
-    }
+    val code: String = errorCode.code
 
 }
