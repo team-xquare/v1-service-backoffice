386560ab1534071c38894f46cf3a3e96ba631e99
leeseojune53 <sung07288346@gmail.com>
2022-09-28 00:05:29 +0900

    ⚡️ :: Hibernate Bug로 버전 업

--- buildSrc/src/main/kotlin/PluginVersions.kt
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -1,5 +1,5 @@
 object PluginVersions {
-    const val SPRING_VERSION = "2.6.7"
+    const val SPRING_VERSION = "2.6.8"
     const val DEPENDENCY_MANAGEMENT = "1.0.11.RELEASE"
     const val SPRING_PLUGIN = "1.6.21"
     const val JPA_PLUGIN = "1.6.10"
