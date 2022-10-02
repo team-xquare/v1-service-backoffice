ecb85f68d9736415f9d77e73022b2ad62a201cc3
leeseojune53 <sung07288346@gmail.com>
2022-10-03 02:51:31 +0900

    ⚡️ :: queryRecord Feign

--- src/main/kotlin/io/github/v1servicebackoffice/infrastructure/feign/client/CloudFlareClient.kt
+++ src/main/kotlin/io/github/v1servicebackoffice/infrastructure/feign/client/CloudFlareClient.kt
@@ -1,18 +1,21 @@
 package io.github.v1servicebackoffice.infrastructure.feign.client
 
-import feign.Headers
 import io.github.v1servicebackoffice.infrastructure.feign.dto.ResponseWrapper
 import io.github.v1servicebackoffice.infrastructure.feign.dto.request.PostRecord
 import io.github.v1servicebackoffice.infrastructure.feign.dto.response.PostRecordResponse
+import io.github.v1servicebackoffice.infrastructure.feign.dto.response.QueryRecordResponse
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestHeader
 
 @FeignClient(name = "CloudFlareClient", url = "https://api.cloudflare.com/client/v4")
 interface CloudFlareClient {
 
     @PostMapping("/zones/\${cloudflare.zone}/dns_records")
     fun createRecord(dnsRecord: PostRecord): ResponseWrapper<PostRecordResponse>
+
+    @GetMapping("/zones/\${cloudflare.zone}/dns_records")
+    fun queryRecord(): ResponseWrapper<List<QueryRecordResponse>>
 
 
 }
