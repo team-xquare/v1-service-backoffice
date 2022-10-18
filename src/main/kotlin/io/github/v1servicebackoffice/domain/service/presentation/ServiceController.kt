64f272ecff60398557cb6b85a090879f459dbcd7
leeseojune53 <sung07288346@gmail.com>
2022-10-18 19:29:04 +0900

    📑 :: GET /

---
+++ src/main/kotlin/io/github/v1servicebackoffice/domain/service/presentation/ServiceController.kt
@@ -0,0 +1,15 @@
+package io.github.v1servicebackoffice.domain.service.presentation
+
+import io.github.v1servicebackoffice.domain.service.service.ServiceService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ServiceController(
+    private val serviceService: ServiceService
+) {
+
+    @GetMapping
+    fun queryServiceList() = serviceService.queryServiceList()
+
+}
