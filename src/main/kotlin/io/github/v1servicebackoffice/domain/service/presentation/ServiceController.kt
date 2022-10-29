8f4051828953a7d2d1fa7997aa05c25db25ad3a5
leeseojune53 <sung07288346@gmail.com>
2022-10-29 12:26:46 +0900

    ⚡️ :: DELETE /{serviceId}

--- src/main/kotlin/io/github/v1servicebackoffice/domain/service/presentation/ServiceController.kt
+++ src/main/kotlin/io/github/v1servicebackoffice/domain/service/presentation/ServiceController.kt
@@ -2,8 +2,13 @@ package io.github.v1servicebackoffice.domain.service.presentation
 
 import io.github.v1servicebackoffice.domain.service.presentation.dto.response.QueryServiceListResponse
 import io.github.v1servicebackoffice.domain.service.service.ServiceService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 class ServiceController(
@@ -12,5 +17,9 @@ class ServiceController(
 
     @GetMapping
     fun queryServiceList(): QueryServiceListResponse = serviceService.queryServiceList()
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{serviceId}")
+    fun removeService(@PathVariable("serviceId") serviceId: UUID) = serviceService.removeService(serviceId)
 
 }
