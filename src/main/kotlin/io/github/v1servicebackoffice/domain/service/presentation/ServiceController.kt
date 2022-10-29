3de4ccd5ad88ba0fa0738e1dd51bb7e54af7416b
leeseojune53 <sung07288346@gmail.com>
2022-10-29 12:18:09 +0900

    ⚡️ :: POST /

--- src/main/kotlin/io/github/v1servicebackoffice/domain/service/presentation/ServiceController.kt
+++ src/main/kotlin/io/github/v1servicebackoffice/domain/service/presentation/ServiceController.kt
@@ -1,9 +1,15 @@
 package io.github.v1servicebackoffice.domain.service.presentation
 
+import io.github.v1servicebackoffice.domain.service.presentation.dto.request.PostServiceRequest
 import io.github.v1servicebackoffice.domain.service.presentation.dto.response.QueryServiceListResponse
 import io.github.v1servicebackoffice.domain.service.service.ServiceService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 class ServiceController(
@@ -12,5 +18,9 @@ class ServiceController(
 
     @GetMapping
     fun queryServiceList(): QueryServiceListResponse = serviceService.queryServiceList()
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    fun postService(@RequestBody @Valid request: PostServiceRequest) = serviceService.postService(request)
 
 }
